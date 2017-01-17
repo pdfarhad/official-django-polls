9ba985147392bd0bf542205b8da558855be24e8c
pdfarhad <pdcsedu@gmail.com>
2017-01-17 15:53:07 -0800

    details added on chapter 3

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,7 @@ class Question(models.Model):
     question_text =  models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+
 class Choice(models.Model):
 
     def __str__(self):
@@ -21,4 +22,4 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-# Create your models here.
+
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 
 def index(request):
-    return HttpResponse("Hello, pd. you are at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request,question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Quesion does not exist")
+    # return render(request, 'polls/detail.html', {'question':question})
+    # # return HttpResponse("You are looking at question %s" % question_id)
+
+
+def results(request,question_id):
+    response = "You are looking at results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
