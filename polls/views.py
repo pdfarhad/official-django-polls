441f994ce973aea52fd6283d84bb266033c231ff
pdfarhad <pdcsedu@gmail.com>
2017-01-18 15:53:09 -0800

    static folder added

--- polls/views.py
+++ polls/views.py
@@ -1,35 +1,49 @@
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse,HttpResponseRedirect
+from django.views.generic.list import ListView
+
+from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+class IndexVIew(generic.ListView):
+
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
 
 
-def detail(request,question_id):
+class DetailView(generic.DetailView):
 
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Quesion does not exist")
-    # return render(request, 'polls/detail.html', {'question':question})
-    # # return HttpResponse("You are looking at question %s" % question_id)
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request,question_id):
-    response = "You are looking at results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk= question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
